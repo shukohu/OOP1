7ba7a8a92ac40f7118b06e97838330475d8808fb
shu <ntyan2002@gmail.com>
2025-02-22 16:43:48 +0800

    streamAPI searchEngine changes

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -18,6 +18,7 @@ public class SearchEngine {
     }
 
     public Set<Searchable> search(String term) {
+
         return searchableItems.stream().filter(item -> item.getSearchTerm().toLowerCase().contains(term.toLowerCase())).collect(Collectors.toCollection(() -> new TreeSet<>(new SearchableComparator())));
     }
 
